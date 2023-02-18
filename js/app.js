26714351a3a2d3571a6d839673c1b48bf64ec9f6
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-18 15:00:13 -0800

    added funcThree

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-//   let total = a + b;
-//   return [total, `The sum of 4 and 7 is ${total}.`];
-// }
+function sum(a, b, c) { //eslint-disable-line
+  let total = a + b;
+  return [total, `The sum of 4 and 7 is ${total}.`];
+}
 
 // // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,12 +27,12 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b, c) { //eslint-disable-line
   let product = a * b;
   return [product, `The product of ${a} and ${b} is ${product}.`];
 }
 
-// Here is the test for multiply(); uncomment it to run it
+// // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -50,11 +50,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let allSum = a + b + c; 
+  let product = a * b * c;
+  return [allSum, product, `${a} and ${b} and ${c} sum to ${allSum}.`, `The product of ${a} and ${b} and ${c} is ${product}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
