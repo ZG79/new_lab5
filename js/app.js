f86207326d404bd98d9db6aa4c4248e3b90594ca
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-02-21 21:21:32 -0800

    function three changed

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b, c) { //eslint-disable-line
+function sum(a, b) { //eslint-disable-line
   let total = a + b;
   return [total, `The sum of 4 and 7 is ${total}.`];
 }
@@ -50,9 +50,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  let allSum = a + b + c; 
-  let product = a * b * c;
-  return [allSum, product, `${a} and ${b} and ${c} sum to ${allSum}.`, `The product of ${a} and ${b} and ${c} is ${product}.`];
+  let allSum = sum(a, b); 
+  let sumTwo = sum(allSum[0],c);
+  let product = multiply(a,b);
+  let productTwo = multiply(product[0], c);
+  return [sumTwo[0], productTwo[0], `${a} and ${b} and ${c} sum to ${sumTwo[0]}.`, `The product of ${a} and ${b} and ${c} is ${productTwo[0]}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
